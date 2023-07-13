ca9f46b5f1610dca0963c23be808342372238ff1
Josh Daniel <joshxfi.dev@gmail.com>
2023-07-13 22:50:26 +0800

    chore: adjust image size

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -27,15 +27,16 @@ export const About = () => {
         className="opacity-20 w-full h-full absolute top-20 pointer-events-none"
       />
       <Container className="mt-24 z-20 relative">
-        <div className="flex md:flex-row gap-10 flex-col items-center">
-          <Image
-            src="/images/logo.png"
-            quality={100}
-            width={250}
-            height={250}
-            className="object-contain pointer-events-none"
-            alt="Google Awards Logo"
-          />
+        <div className="flex md:flex-row flex-col items-center">
+          <div className="relative w-[150px] h-[150px] md:w-[250px] md:h-[250px] flex-none mb-16 md:mb-0">
+            <Image
+              src="/images/logo.png"
+              quality={100}
+              fill
+              className="object-contain pointer-events-none"
+              alt="Google Awards Logo"
+            />
+          </div>
           <div className="flex flex-col gap-y-10">
             <p className="md:text-2xl text-xl leading-normal text-gray-100 lg:w-3/4 w-full">
               &quot;Google Awards: A testament to the power of recognition,
