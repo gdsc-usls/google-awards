ccde98571cb36ee0a4b82129a2759a49c7063b5b
Josh Daniel <joshxfi.dev@gmail.com>
2023-07-14 15:46:40 +0800

    feat: update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,19 @@ export const metadata: Metadata = {
   title: "Google Awards",
   description:
     "A testament to the power of recognition, fueling passion and driving excellence. By honoring outstanding individuals, we inspire a ripple effect of innovation, empower dreams, and shape a brighter future together.",
+  openGraph: {
+    type: "website",
+    title: "Google Awards",
+    description:
+      "A testament to the power of recognition, fueling passion and driving excellence. By honoring outstanding individuals, we inspire a ripple effect of innovation, empower dreams, and shape a brighter future together.",
+    url: "https://awards.gdsc-usls.live/",
+    images: [
+      "https://user-images.githubusercontent.com/69457996/252296037-50595eb1-1886-4062-b7e1-2b28a94e385c.png",
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+  },
 };
 
 export default function RootLayout({
