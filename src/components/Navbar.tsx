425d57f423e13117164e403512727234f90da2b3
Josh Daniel <joshxfi.dev@gmail.com>
2023-07-14 16:06:38 +0800

    chore: add link to logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,10 @@ import { Container } from "./Container";
 export const Navbar = () => {
   return (
     <Container className="flex justify-between mt-16 z-30 items-center">
-      <div className="flex gap-x-1 items-center border border-[#454545] lg:px-8 px-5 py-2 bg-secondary-400 rounded-full">
+      <Link
+        href="/"
+        className="flex gap-x-1 items-center border border-[#454545] lg:px-8 px-5 py-2 bg-secondary-400 rounded-full"
+      >
         <Image
           priority
           src="/images/logo.png"
@@ -15,7 +18,7 @@ export const Navbar = () => {
           alt="Google Awards Logo"
         />
         <p>Google Awards</p>
-      </div>
+      </Link>
 
       <ul className="flex gap-x-24 lg:flex-row flex-col cursor-pointer lg:bg-secondary-400 lg:p-4 lg:px-16 lg:rounded-full lg:border lg:border-[#454545]">
         <li>
