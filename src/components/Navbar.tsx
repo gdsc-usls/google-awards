49ed282b2e319458b61f58ce7fc96d5bc169376b
Josh Daniel <joshxfi.dev@gmail.com>
2023-07-14 15:29:34 +0800

    feat: add background in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 import { Container } from "./Container";
 
 export const Navbar = () => {
   return (
-    <Container className="flex justify-between items-start mt-16 z-30">
-      <div className="flex gap-x-1 items-center border border-[#454545] lg:px-8 px-5 py-2 rounded-full">
+    <Container className="flex justify-between mt-16 z-30 items-center">
+      <div className="flex gap-x-1 items-center border border-[#454545] lg:px-8 px-5 py-2 bg-secondary-400 rounded-full">
         <Image
           priority
           src="/images/logo.png"
@@ -16,10 +17,16 @@ export const Navbar = () => {
         <p>Google Awards</p>
       </div>
 
-      <ul className="flex gap-x-24 lg:flex-row flex-col cursor-pointer mix-blend-difference">
-        <li>Home</li>
-        <li>About</li>
-        <li>GitHub</li>
+      <ul className="flex gap-x-24 lg:flex-row flex-col cursor-pointer lg:bg-secondary-400 lg:p-4 lg:px-16 lg:rounded-full lg:border lg:border-[#454545]">
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="https://github.com/gdsc-usls/google-awards">GitHub</a>
+        </li>
       </ul>
     </Container>
   );
