e1dc8b38a9fef6b16b1e94c87ea89de6f7bd83ee
Josh Daniel <joshxfi.dev@gmail.com>
2023-07-14 23:43:33 +0800

    feat: implement route to certificate

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,13 +1,27 @@
 "use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export const Form = () => {
+  const router = useRouter();
+  const [id, setId] = useState("");
+
   return (
-    <form className="mt-12 flex gap-x-3 h-16 md:text-xl text-lg">
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        router.push(`/for/${id}`);
+      }}
+      className="mt-12 flex gap-x-3 h-16 md:text-xl text-lg"
+    >
       <input
         required
+        minLength={7}
         maxLength={7}
         type="text"
         placeholder="Enter Student ID"
+        value={id}
+        onChange={(e) => setId(e.target.value)}
         className="border border-secondary-200 px-8 w-full h-full md:max-w-[300px] rounded text-secondary-100 bg-[#121212] outline-none placeholder:text-secondary-100"
       />
 
