18479df4ab95cfe7318021cb3d03796fe067a697
Josh Daniel <joshxfi.dev@gmail.com>
2023-07-15 21:56:29 +0800

    feat: add certificate list

--- src/app/for/[id]/page.tsx
+++ src/app/for/[id]/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import Image from "next/image";
+import { useState } from "react";
 import Tilt from "react-parallax-tilt";
 
-import { useAward } from "@/hooks";
+import { Award, useAward } from "@/hooks";
 import { Container } from "@/components";
 
 export default function Award({ params }: { params: { id: string } }) {
+  const [certData, setCertData] = useState<Award | null>();
   const { data } = useAward(params.id);
 
   return (
@@ -16,6 +18,42 @@ export default function Award({ params }: { params: { id: string } }) {
         fill
         className="opacity-20 w-[1500px] h-full absolute top-0 -z-10"
       />
+      {certData ? (
+        <Certificate data={certData} setData={setCertData} />
+      ) : (
+        <div className="flex gap-x-4">
+          {data.map((item: Award) => (
+            <Card key={item.award} data={item} setData={setCertData} />
+          ))}
+        </div>
+      )}
+    </Container>
+  );
+}
+
+type Props = {
+  data: Award;
+  setData: React.Dispatch<React.SetStateAction<Award | undefined | null>>;
+};
+
+const Card = ({ data, setData }: Props) => {
+  return (
+    <button
+      onClick={() => setData(data)}
+      className="bg-secondary-300 border text-left border-secondary-200 px-8 py-6 rounded-md bg-opacity-60 transition-all hover:-translate-y-4 duration-300 w-[300px] cursor-pointer flex flex-col justify-between h-[180px]"
+    >
+      <div>
+        <h1 className="text-primary-100 text-xl">{data.name}</h1>
+        <h1>{data.award}</h1>
+      </div>
+      <p className="text-gray-400 text-sm">View Certificate &rarr;</p>
+    </button>
+  );
+};
+
+const Certificate = ({ data, setData }: Props) => {
+  return (
+    <div className="flex flex-col gap-y-20 items-center">
       <Tilt gyroscope className="max-w-[850px] mx-auto">
         <div className="w-full relative rounded-lg grid place-items-center">
           <p className="absolute lg:mb-32 md:mb-28 sm:mb-24 mb-16 uppercase text-[#C6C6C6] md:[font-size:clamp(14px,3vw,30px)] [font-size:clamp(14px,3vw,30px)]">
@@ -31,6 +69,13 @@ export default function Award({ params }: { params: { id: string } }) {
           />
         </div>
       </Tilt>
-    </Container>
+      <button
+        type="button"
+        onClick={() => setData(null)}
+        className="border border-secondary-200 rounded-full px-8 py-3 hover:scale-105 transform transition-all duration-200 bg-secondary-300 bg-opacity-60"
+      >
+        Back to List
+      </button>
+    </div>
   );
-}
+};
